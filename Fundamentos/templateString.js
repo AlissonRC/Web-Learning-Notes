12c840f7e8147e92a0a8040c1c6c5ee985538fb1
AlissonRC <alissonrdacruz@gmail.com>
2019-06-03 20:22:50 -0300

    fundamentos

---
+++ Fundamentos/templateString.js
@@ -0,0 +1,10 @@
+const nome = "Alisson"
+const concatena = "Olá " + nome
+const template = `Olá
+ ${nome}`
+
+ const up = texto => texto.toUpperCase() 
+console.log(concatena)
+console.log(template)
+console.log(`1+1 = ${1+1}`)
+console.log(up("teste"))
